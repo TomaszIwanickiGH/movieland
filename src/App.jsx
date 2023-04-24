5571f983ea52db6aa91ee0784c50f9d75ae10ceb
Tomasz <tomasz.iwanicki@onet.pl>
2023-04-24 19:29:43 +0200

    Add some divs and styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import React from 'react'
 import { Navbar, SearchBar, HomeWrapper, Movies, TvSeries, Bookmarked } from './components'
 
 import { Route, Routes } from 'react-router-dom'
+import { styles } from './constants'
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <SearchBar />
-      <Routes>
-        <Route path="/" element={<HomeWrapper />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/tv-series" element={<TvSeries />} />
-        <Route path="/bookmarked" element={<Bookmarked />} />
-      </Routes>
+    <div className={`${styles.padding} bg-primary`}>
+      <div className="flex gap-12">
+        <Navbar />
+        <div>
+          <SearchBar />
+          <Routes>
+            <Route path="/" element={<HomeWrapper />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/tv-series" element={<TvSeries />} />
+            <Route path="/bookmarked" element={<Bookmarked />} />
+          </Routes>
+        </div>
+      </div>
     </div>
   )
 }
