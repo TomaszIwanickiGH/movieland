bf7e8828330c66bda326d4e013f9cd013f096c1f
Tomasz <tomasz.iwanicki@onet.pl>
2023-04-26 18:21:32 +0200

    Comlete section for movies/series

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { styles } from '../constants'
+import data from '../constants/data.json'
+
+import { Card } from '../components'
 
 const Movies = () => {
-  return <div>Movies</div>
+  return (
+    <section className={`${styles.paddingSmall} flex flex-col gap-6 relative mt-4 md:mt-0`}>
+      <h2 className="text-white text-[32px] font-[500]">Movies</h2>
+      <div className="flex flex-wrap xl:justify-start justify-center md:gap-8 gap-4">
+        {data.map((data) => {
+          if (data.category === 'Movie') return <Card key={data.title} image={data.thumbnail.regular.small} isMarked={data.isBookmarked} {...data} />
+        })}
+      </div>
+    </section>
+  )
 }
 
 export default Movies
--- src/components/TvSeries.jsx
+++ src/components/TvSeries.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { styles } from '../constants'
+import data from '../constants/data.json'
+
+import { Card } from '../components'
 
 const TvSeries = () => {
-  return <div>TvSeries</div>
+  return (
+    <section className={`${styles.paddingSmall} flex flex-col gap-6 relative mt-4 md:mt-0`}>
+      <h2 className="text-white text-[32px] font-[500]">TV Series</h2>
+      <div className="flex flex-wrap xl:justify-start justify-center md:gap-8 gap-4">
+        {data.map((data) => {
+          if (data.category === 'TV Series') return <Card key={data.title} image={data.thumbnail.regular.small} isMarked={data.isBookmarked} {...data} />
+        })}
+      </div>
+    </section>
+  )
 }
 
 export default TvSeries
