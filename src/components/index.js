a0757bb38f16d88e174b8a9f42b534dff189ee8b
Tomasz <tomasz.iwanicki@onet.pl>
2023-04-26 18:13:18 +0200

    Import new Card component

--- src/components/index.js
+++ src/components/index.js
@@ -8,5 +8,6 @@ import Bookmarked from './Bookmarked'
 import HomeWrapper from './HomeWrapper'
 import BookmarkedMovies from './BookmarkedMovies'
 import BookmarkedTvSeries from './BookmarkedTvSeries'
+import Card from './Card'
 
-export { Navbar, SearchBar, Trending, Recommended, Movies, TvSeries, Bookmarked, HomeWrapper, BookmarkedMovies, BookmarkedTvSeries }
+export { Navbar, SearchBar, Trending, Recommended, Movies, TvSeries, Bookmarked, HomeWrapper, BookmarkedMovies, BookmarkedTvSeries, Card }
