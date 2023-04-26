97560c876e8fcc0e09f438e5198496ba8436149c
Tomasz <tomasz.iwanicki@onet.pl>
2023-04-26 18:13:49 +0200

    Small changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className="flex lg:flex-col flex-row justify-between bg-darkBlue rounded-lg lg:w-fit p-6">
+    <nav className="flex lg:flex-col flex-row justify-between items-center bg-darkBlue rounded-lg lg:w-fit xl:min-w-[100px] p-6 lg:h-[90vh]">
       <Link to="/">
         <img src={images.logo} alt="logo" className="sm:w-[40px] w-[30px] sm:h-[35px] h-[30px]" />
       </Link>
 
-      <ul className="flex lg:flex-col flex-row list-none sm:gap-10 gap-8 items-center lg:mt-16">
+      <ul className="flex lg:flex-col flex-row list-none sm:gap-10 gap-8 items-center lg:mt-[-8rem]">
         {utils.map((nav) => (
           <Link key={nav.path} to={nav.path}>
             <li className="hover:opacity-90">
@@ -19,7 +19,7 @@ const Navbar = () => {
         ))}
       </ul>
 
-      <img src={images.imageAvatar} alt="avatar" className="sm:w-[40px] h-[30px] sm:h-[40px] w-[30px] lg:mt-80" />
+      <img src={images.imageAvatar} alt="avatar" className="sm:w-[40px] h-[30px] sm:h-[40px] w-[30px] lg:mt-70" />
     </nav>
   )
 }
