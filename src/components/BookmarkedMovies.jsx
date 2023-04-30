7ed5f71c06113e2a8b2e410de940a16339711200
Tomasz <tomasz.iwanicki@onet.pl>
2023-04-30 20:05:44 +0200

    Display the information of results found by searching

--- src/components/BookmarkedMovies.jsx
+++ src/components/BookmarkedMovies.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { styles } from '../constants'
 import data from '../constants/data.json'
 
@@ -9,9 +9,32 @@ import { useSelector } from 'react-redux'
 const BookmarkedMovies = () => {
   const search = useSelector((state) => state.search)
 
+  let countItems = []
+  let countSeries = []
+
+  useEffect(() => {
+    countItems = []
+    countSeries = []
+  }, [search])
+
   return (
     <section className={`${styles.paddingSmall} flex flex-col gap-6 relative mt-4 md:mt-0`}>
       <h2 className="text-white text-[32px] font-[500]">Bookmarked Movies</h2>
+
+      {data.map((data) => {
+        if (data.category === 'Movie' && data.isBookmarked && data.title.includes(search)) countItems.push(data.title)
+      })}
+
+      {countItems.length === 0 ? (
+        <div className="text-white text-[24px]">No results found for '{search}'</div>
+      ) : search.length > 0 ? (
+        <div className="text-white text-[24px]">
+          Found {countItems.length} results for '{search}'
+        </div>
+      ) : (
+        <div></div>
+      )}
+
       <div className="flex flex-wrap xl:justify-start justify-center md:gap-8 gap-4">
         {data.map((data) => {
           if (data.category === 'Movie' && data.isBookmarked && data.title.includes(search)) return <Card key={data.title} image={data.thumbnail.regular.small} isMarked={data.isBookmarked} {...data} />
@@ -19,6 +42,21 @@ const BookmarkedMovies = () => {
       </div>
 
       <h2 className="text-white text-[32px] font-[500]">Bookmarked TV Series</h2>
+
+      {data.map((data) => {
+        if (data.category === 'TV Series' && data.isBookmarked && data.title.includes(search)) countSeries.push(data.title)
+      })}
+
+      {countSeries.length === 0 ? (
+        <div className="text-white text-[24px]">No results found for '{search}'</div>
+      ) : search.length > 0 ? (
+        <div className="text-white text-[24px]">
+          Found {countSeries.length} results for '{search}'
+        </div>
+      ) : (
+        <div></div>
+      )}
+
       <div className="flex flex-wrap xl:justify-start justify-center md:gap-8 gap-4">
         {data.map((data) => {
           if (data.category === 'TV Series' && data.isBookmarked && data.title.includes(search)) return <Card key={data.title} image={data.thumbnail.regular.small} isMarked={data.isBookmarked} {...data} />
--- src/components/TvSeries.jsx
+++ src/components/TvSeries.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { styles } from '../constants'
 import data from '../constants/data.json'
 
@@ -9,9 +9,30 @@ import { useSelector } from 'react-redux'
 const TvSeries = () => {
   const search = useSelector((state) => state.search)
 
+  let countItems = []
+
+  useEffect(() => {
+    countItems = []
+  }, [search])
+
   return (
     <section className={`${styles.paddingSmall} flex flex-col gap-6 relative mt-4 md:mt-0`}>
       <h2 className="text-white text-[32px] font-[500]">TV Series</h2>
+
+      {data.map((data) => {
+        if (data.category === 'TV Series' && data.title.includes(search)) countItems.push(data.title)
+      })}
+
+      {countItems.length === 0 ? (
+        <div className="text-white text-[24px]">No results found for '{search}'</div>
+      ) : search.length > 0 ? (
+        <div className="text-white text-[24px]">
+          Found {countItems.length} results for '{search}'
+        </div>
+      ) : (
+        <div></div>
+      )}
+
       <div className="flex flex-wrap xl:justify-start justify-center md:gap-8 gap-4">
         {data.map((data) => {
           if (data.category === 'TV Series' && data.title.includes(search)) return <Card key={data.title} image={data.thumbnail.regular.small} isMarked={data.isBookmarked} {...data} />
