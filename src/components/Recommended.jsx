e58ebd1b32f9863aa3823544e25e44627f17f558
Tomasz <tomasz.iwanicki@onet.pl>
2023-04-30 18:14:14 +0200

    Add search functionality

--- src/components/Recommended.jsx
+++ src/components/Recommended.jsx
@@ -4,13 +4,17 @@ import data from '../constants/data.json'
 
 import { Card } from '../components'
 
+import { useSelector } from 'react-redux'
+
 const Recommended = () => {
+  const search = useSelector((state) => state.search)
+
   return (
     <section className={`${styles.paddingSmall} flex flex-col gap-6 relative mt-4 md:mt-0`}>
       <h2 className="text-white text-[32px] font-[500]">Recommended</h2>
       <div className="flex flex-wrap xl:justify-start justify-center md:gap-8 gap-4">
         {data.map((data) => {
-          if (!data.thumbnail.trending) return <Card key={data.title} image={data.thumbnail.regular.small} isMarked={data.isBookmarked} {...data} />
+          if (!data.thumbnail.trending && data.title.includes(search)) return <Card key={data.title} image={data.thumbnail.regular.small} isMarked={data.isBookmarked} {...data} />
         })}
       </div>
     </section>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { images, styles } from '../constants'
 
+import { useDispatch } from 'react-redux'
+
 const SearchBar = () => {
+  const [value, setValue] = useState('')
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  const dispatch = useDispatch()
+
+  const updateSearch = () => dispatch({ type: 'SEARCH', payload: value })
+
+  useEffect(() => {
+    updateSearch()
+  }, [value])
+
   return (
     <section className={`${styles.paddingSmall} flex gap-4 items-center`}>
       <img src={images.iconSearch} alt="search" className="w-[24px] h-[24px]" />
-      <input type="text" placeholder="Search for movies or TV series" className="bg-primary outline-none w-full md:min-w-[300px] min-w-[220px] text-white focus:border-b-lightGray border-b-2 border-b-primary py-2 hover:cursor-pointer" />
+      <input type="text" placeholder="Search for movies or TV series" className="bg-primary outline-none w-full md:min-w-[300px] min-w-[220px] text-white focus:border-b-lightGray border-b-2 border-b-primary py-2 hover:cursor-pointer" value={value} onChange={handleChange} />
     </section>
   )
 }
