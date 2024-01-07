1f05b132f8653474587fd42f77a3faf5f88e6fa4
Tomasz <tomasz.iwanicki@onet.pl>
2024-01-07 18:22:31 +0100

    Give navbar fixed position

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
-import { Navbar, SearchBar, HomeWrapper, Movies, TvSeries, Bookmarked } from './components'
+import React from 'react';
+import { Navbar, SearchBar, HomeWrapper, Movies, TvSeries, Bookmarked } from './components';
 
-import { Route, Routes } from 'react-router-dom'
-import { styles } from './constants'
+import { Route, Routes } from 'react-router-dom';
+import { styles } from './constants';
 
 const App = () => {
   return (
-    <div className={`${styles.padding} bg-primary h-[100vh]`}>
+    <div className={`${styles.padding} bg-primary h-[100vh] relative`}>
       <div className="flex lg:flex-row flex-col gap-8">
-        <Navbar />
+        <div className="min-w-[100px]">
+          <div className="lg:fixed">
+            <Navbar />
+          </div>
+        </div>
         <div className="w-full">
           <SearchBar />
           <Routes>
-            <Route path="/" element={<HomeWrapper />} />
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/tv-series" element={<TvSeries />} />
-            <Route path="/bookmarked" element={<Bookmarked />} />
+            <Route
+              path="/"
+              element={<HomeWrapper />}
+            />
+            <Route
+              path="/movies"
+              element={<Movies />}
+            />
+            <Route
+              path="/tv-series"
+              element={<TvSeries />}
+            />
+            <Route
+              path="/bookmarked"
+              element={<Bookmarked />}
+            />
           </Routes>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,57 @@
-import React, { useState } from 'react'
-import { images, utils } from '../constants'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { images, utils } from '../constants';
+import { Link } from 'react-router-dom';
 
-import LogoConverter from './LogoConverter'
+import LogoConverter from './LogoConverter';
 
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
-  const [active, setActive] = useState(1)
+  const [active, setActive] = useState(1);
 
   return (
-    <motion.nav initial={{ opacity: 0, x: '-30%' }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 1.5 }} className="flex lg:flex-col flex-row justify-between items-center bg-darkBlue rounded-lg lg:w-fit xl:min-w-[100px] p-6 lg:h-[90vh]">
+    <motion.nav
+      initial={{ opacity: 0, x: '-30%' }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 1.5 }}
+      className="flex lg:flex-col flex-row justify-between items-center bg-darkBlue rounded-lg lg:w-fit xl:min-w-[100px] p-6 lg:h-[90vh]"
+    >
       <Link to="/">
-        <img src={images.logo} alt="logo" className="sm:w-[40px] w-[30px] sm:h-[35px] h-[30px]" />
+        <img
+          src={images.logo}
+          alt="logo"
+          className="sm:w-[40px] w-[30px] sm:h-[35px] h-[30px]"
+        />
       </Link>
 
       <ul className="flex lg:flex-col flex-row list-none sm:gap-10 gap-8 items-center lg:mt-[-8rem]">
         {utils.map((nav) => (
-          <Link key={nav.path} to={nav.path}>
+          <Link
+            key={nav.path}
+            to={nav.path}
+          >
             <li
               className="hover:opacity-50"
               onClick={() => {
-                setActive(nav.id)
+                setActive(nav.id);
               }}
             >
-              <LogoConverter {...nav} fill={active === nav.id ? '#fff' : '#5A698F'} />
+              <LogoConverter
+                {...nav}
+                fill={active === nav.id ? '#fff' : '#5A698F'}
+              />
             </li>
           </Link>
         ))}
       </ul>
 
-      <img src={images.imageAvatar} alt="avatar" className="sm:w-[40px] h-[30px] sm:h-[40px] w-[30px] lg:mt-70" />
+      <img
+        src={images.imageAvatar}
+        alt="avatar"
+        className="sm:w-[40px] h-[30px] sm:h-[40px] w-[30px] lg:mt-70"
+      />
     </motion.nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
