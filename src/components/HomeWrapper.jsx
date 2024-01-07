7dbdbffff90c0956a656ed9cb3be42b4afe5890f
Tomasz <tomasz.iwanicki@onet.pl>
2024-01-07 18:22:48 +0100

    Set width to 100%

--- src/components/HomeWrapper.jsx
+++ src/components/HomeWrapper.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { Trending, Recommended } from './index'
+import React from 'react';
+import { Trending, Recommended } from './index';
 
 const HomeWrapper = () => {
   return (
-    <div className="md:mt-[-1rem] mt-4">
+    <div className="md:mt-[-1rem] mt-4 w-full">
       <Trending />
       <Recommended />
     </div>
-  )
-}
+  );
+};
 
-export default HomeWrapper
+export default HomeWrapper;
