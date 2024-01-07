180df33d121285cc74a2faaf79282e23263cf02a
Tomasz <tomasz.iwanicki@onet.pl>
2024-01-07 18:21:56 +0100

    Fix width of Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,38 @@
-import React, { useState } from 'react'
-import { images } from '../constants'
+import React, { useState } from 'react';
+import { images } from '../constants';
 
 const Card = ({ image, title, category, year, rating, isMarked }) => {
-  const [toggle, setToggle] = useState(isMarked)
+  const [toggle, setToggle] = useState(isMarked);
   return (
-    <div id="trending" className={`flex flex-col gap-2 md:w-[280px] w-[164px] hover:cursor-pointer`}>
+    <div
+      id="trending"
+      className={`flex flex-col gap-2 hover:cursor-pointer w-full`}
+    >
       <div className="relative card-bg">
-        <img src={image} className="md:w-[280px] w-[164px] md:h-[174px] h-[110px] rounded-md card-bg" />
-        <div id="show" className="hidden show-button">
+        <img
+          src={image}
+          className="rounded-md card-bg w-full"
+        />
+        <div
+          id="show"
+          className="hidden show-button"
+        >
           <div className="flex items-center justify-center bg-lightGray rounded-3xl button-opacity">
-            <img src={images.iconPlay} alt="play" />
+            <img
+              src={images.iconPlay}
+              alt="play"
+            />
           </div>
         </div>
-        <div className="flex items-center justify-center bg-black hover:bg-lightGray hover:opacity-100 opacity-60 rounded-full w-[32px] h-[32px] absolute top-[10px] right-[10px]" onClick={() => setToggle(!toggle)}>
-          <img src={toggle ? images.iconBookmarkFull : images.iconBookmarkEmpty} alt="" className="w-[11px] h-[14px]" />
+        <div
+          className="flex items-center justify-center bg-black hover:bg-lightGray hover:opacity-100 opacity-60 rounded-full w-[32px] h-[32px] absolute top-[10px] right-[10px]"
+          onClick={() => setToggle(!toggle)}
+        >
+          <img
+            src={toggle ? images.iconBookmarkFull : images.iconBookmarkEmpty}
+            alt=""
+            className="w-[11px] h-[14px]"
+          />
         </div>
       </div>
 
@@ -22,7 +41,11 @@ const Card = ({ image, title, category, year, rating, isMarked }) => {
           <p className="text-gray-300 md:text-[16px] text-[13px]">{year}</p>
           <p className="font-bold text-gray-300 md:text-[16px] text-[13px]"> · </p>
           <p className="flex items-center justify-center gap-1 text-gray-300 md:text-[16px] text-[13px]">
-            <img src={category === 'Movie' ? images.iconCategoryMovie : images.iconCategoryTv} className="w-[12px] h-[12px]" /> {category}
+            <img
+              src={category === 'Movie' ? images.iconCategoryMovie : images.iconCategoryTv}
+              className="w-[12px] h-[12px]"
+            />{' '}
+            {category}
           </p>
           <p className="font-bold text-gray-300 md:text-[16px] text-[13px]"> · </p>
           <p className="text-gray-300 md:text-[16px] text-[13px]">{rating}</p>
@@ -30,7 +53,7 @@ const Card = ({ image, title, category, year, rating, isMarked }) => {
         <h2 className="text-white text-[18px] font-[600] tracking-[1.5px]">{title}</h2>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- src/components/Recommended.jsx
+++ src/components/Recommended.jsx
@@ -1,32 +1,35 @@
-import React, { useEffect } from 'react'
-import { styles } from '../constants'
-import data from '../constants/data.json'
+import React, { useEffect } from 'react';
+import { styles } from '../constants';
+import data from '../constants/data.json';
 
-import { Card } from '../components'
+import { Card } from '../components';
 
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
 
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion';
 
 const Recommended = () => {
-  const search = useSelector((state) => state.search)
+  const search = useSelector((state) => state.search);
 
-  let countItems = []
+  let countItems = [];
 
   useEffect(() => {
-    countItems = []
-  }, [search])
+    countItems = [];
+  }, [search]);
 
   return (
     <section className={`${styles.paddingSmall} flex flex-col gap-6 relative mt-4 md:mt-0`}>
-      <motion.h2 initial={{ opacity: 0, x: '-30%' }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 1 }} className="text-white text-[32px] font-[500]">
+      <motion.h2
+        initial={{ opacity: 0, x: '-30%' }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+        className="text-white text-[32px] font-[500]"
+      >
         Recommended
       </motion.h2>
-
       {data.map((data) => {
-        if (!data.thumbnail.trending && data.title.includes(search)) countItems.push(data.title)
+        if (!data.thumbnail.trending && data.title.includes(search)) countItems.push(data.title);
       })}
-
       {countItems.length === 0 ? (
         <div className="text-white text-[24px]">No results found for '{search}'</div>
       ) : search.length > 0 ? (
@@ -36,14 +39,22 @@ const Recommended = () => {
       ) : (
         <div></div>
       )}
-
-      <div className="flex flex-wrap xl:justify-start justify-center md:gap-8 gap-4">
+      {/* flex flex-wrap xl:justify-start justify-center  */}
+      <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 md:gap-8 gap-4">
         {data.map((data) => {
-          if (!data.thumbnail.trending && data.title.includes(search)) return <Card key={data.title} image={data.thumbnail.regular.small} isMarked={data.isBookmarked} {...data} />
+          if (!data.thumbnail.trending && data.title.includes(search))
+            return (
+              <Card
+                key={data.title}
+                image={data.thumbnail.regular.small}
+                isMarked={data.isBookmarked}
+                {...data}
+              />
+            );
         })}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Recommended
+export default Recommended;
