0917f3cd619e681f8632b6aa4465370f55bd2f64
Tomasz <tomasz.iwanicki@onet.pl>
2024-01-08 18:51:29 +0100

    Remove Trending section when searchbar is not empty

--- src/components/HomeWrapper.jsx
+++ src/components/HomeWrapper.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Trending, Recommended } from './index';
+import useGlobals from '../globals';
 
 const HomeWrapper = () => {
+  const globals = useGlobals();
+  console.log(globals.search === '');
   return (
     <div className="md:mt-[-1rem] mt-4 w-full">
-      <Trending />
+      {globals.search === '' && <Trending />}
       <Recommended />
     </div>
   );
